86b9189d6f0e2b2d1c5bdba48b73123117c43f3c
Mike <m.courbesriviere@orange.fr>
2024-07-22 15:05:21 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,3 @@
+// var  est accessible dans tout le script//    
+// let est accessible uniquement dans le bloc où il est déclaré peut etre modifié mais pas redéclaré//
+//const est une constante qui ne peut pas être modifiée//
